68c1f07d841e70ca4e1cf8b5a78c58b2e6dc275b
llldddbbb <571002217@qq.com>
2017-07-09 14:31:24 +0800

    按钮组件完成

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+    <transition name="fadeRotate">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
@@ -42,12 +46,25 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
-    .cart-decrease, .cart-add
+    .cart-decrease
       display: inline-block
       padding: 6px
-      line-height: 24px
-      font-size: 24px
-      color: rgb(0, 160, 220)
+      transition: all .4s linear
+      .inner
+        line-height: 24px
+        font-size: 24px
+        color: rgb(0, 160, 220)
+        transition all 0.4s linear
+      &.fadeRotate-enter-active, &.fadeRotate-leave-active
+        transform translate3d(0, 0, 0)
+        .inner
+          display: inline-block
+          transform: rotate(0deg)
+      &.fadeRotate-enter, &.fadeRotate-leave-active
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
@@ -57,6 +74,11 @@
       text-align: center
       font-size: 10px
       color: rgb(147, 153, 159)
-
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
 
 </style>
